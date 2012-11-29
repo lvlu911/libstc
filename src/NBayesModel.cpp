a7f21fcff561a275f02cca1a7552a3bdef9a8bb8
Lu Lv <lvlu911@163.com>
2012-11-29 16:58:05 +0800

    modify NBayesModel.cpp

--- src/NBayesModel.cpp
+++ src/NBayesModel.cpp
@@ -1,25 +1,72 @@
 #include "NBayesModel.h"
 
+vector<string> NBayesModel::split(const string& str)
+{
+	vector<string> words;
+	string word;
+	bool inWord = false;
+	for(int i = 0; i < str.size(); i++)
+	{
+		if(str[i] == ' ')
+		{
+			if(inWord)
+			{	
+				words.push_back(word);
+				word.clear();
+				inWord = false;
+			}
+		}
+		else
+		{
+			if(!inWord)
+				inWord = true;
+			word.push_back(str[i]);
+		}
+	}
+}
+
+inline const vector<string> & NBayesModel::getTypes()
+{
+	return types;
+}
+
 double NBayesModel::getFreq(int wordID, const string& docType)
 {
-        T_FREQ_MAP::iterator it1 = freq.find(wordID);
-        if(it1 != freq.end())
-        {    
-                T_SD_MAP::iterator it2 = it1->second.find(docType);
-                if(it2 != it1->second.end())
-                        return *it2;
-                return 1.0 / freqMap.size();
-        }
+	if(typeToIndex.count(docType) > 0)
+	{
+		int index = (typeToIndex.find(docType))->second;
+        	NBayesModel::FREQ_MAP_IT it1 = freqMap.find(wordID);
+        	if(it1 != freqMap.end())
+       		{	    
+                	map<int,double>::iterator it2 = it1->second.find(index);
+                	if(it2 != it1->second.end())
+                        	return it2->second;
+                	return 1.0 / freqMap.size();
+        	}
+		else
+			return 1.0;//neglect this word.
+	}
         return 0.0;
 }
 
 ostream& operator<<(ostream &os, const NBayesModel &model)
 {
-        for(T_FREQ_MAP::iterator it1 = model.begin(); it1 != model.end(); it1++)
+	int len = model.types.size();
+
+	for(int i = 0; i < len; i++)
+		cout << model.types[i] << " ";
+	cout << endl;
+	
+        for(NBayesModel::FREQ_MAP_CON_IT it1 = model.freqMap.begin(); it1 != model.freqMap.end(); it1++)
         {
-                for(T_SD_MAP::iterator it2 = it1->second.beign();
-                        it2 != it1->second.end(); it2++)
-                        os << it2->first <<" " << it2->second << " ";
+		for(int i = 0; i < len; i++)
+		{
+			map<int,double>::const_iterator it2 = it1->second.find(i);
+			if(it2 != it1->second.end())
+				os << it2->second << " ";
+			else
+				os << "0 ";
+		}
                 os << it1->first << endl;
         }
 }
@@ -27,9 +74,31 @@ ostream& operator<<(ostream &os, const NBayesModel &model)
 istream& operator>>(istream &is, NBayesModel &model)
 {
         string line;
+	getline(is, line);
+	model.types = NBayesModel::split(line);
+	int len = model.types.size();
+	for(int i = 0; i < len; i++)
+		model.typeToIndex.insert(make_pair(model.types[i],i));
+		
+	vector<string> v;
         while(getline(is, line))
         {
-                line.find
+		v = NBayesModel::split(line);
+		if(v.size() == len + 1)
+		{
+			int wordID = atoi(v[len].c_str());
+			if(wordID > 0)
+			{
+				map<int,double> m;
+				for(int i = 0; i < len; i++)
+				{
+					double freq = atof(v[i].c_str());
+					if(freq > 0.0)
+						m[i] = freq;
+				}
+				model.freqMap[wordID] = m;
+			}
+		}
         }
 
 }
